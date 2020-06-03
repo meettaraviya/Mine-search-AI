596360ddc597a552f9a68d67bfd225be2fa37fb6
Meet Taraviya <meet.taraviya@cs.ucla.edu>
2020-06-03 00:16:33 -0700

    fixed player bug

--- player.py
+++ player.py
@@ -21,10 +21,6 @@ class GreedyPlayer(Player):
 
         for n_safe_left in range(ms.H*ms.W-ms.N, 0, -1):
 
-            if prev_square == "@":
-                n_safe_left += 1
-                break
-
             if debug:
                 print(f"Safe spots left: {n_safe_left}\n")
                 print("Game grid:\n")
@@ -57,6 +53,9 @@ class GreedyPlayer(Player):
 
             prev_square = ms.reveal(mi, mj)
 
+            if prev_square == "@":
+                break
+
         if debug:
             print("Final game grid:\n")
             ms.print()
@@ -72,10 +71,10 @@ class GreedyPlayer(Player):
                 return True
 
 
-
 class OptimizedGreedyPlayer(Player):
     """For each move, calculate the distribution and make the move corresponding to the position with minimum probability of having a mine"""
     """If there are locations with probability zero, reveal all of them one by one"""
+
     def play(self, ms: MineSweeper, debug: bool = False):
 
         prev_square = None
@@ -121,7 +120,6 @@ class OptimizedGreedyPlayer(Player):
             else:
                 moves = [(mi, mj)]
 
-
             if debug:
                 print(f"Moves = {moves}")
                 print("\n-" + "------"*ms.W+"\n")
@@ -129,7 +127,10 @@ class OptimizedGreedyPlayer(Player):
             for i, j in moves:
                 prev_square = ms.reveal(i, j)
 
-            n_safe_left -= len(moves)
+                if prev_square == "@":
+                    break
+
+                n_safe_left -= 1
 
         if debug:
             print("Final game grid:\n")
